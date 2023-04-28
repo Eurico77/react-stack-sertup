67bfdea0fb5d938e3dbb5451fa4d3b365cc78fde
eurico77 <euricomagalhaes61@gmail.com>
2023-04-27 22:55:48 -0300

    chore: refactor schedule

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
-import { GlobalStyle } from './styles/global';
 
 ReactDOM.render(
   <>
-    <GlobalStyle />
     <App />
   </>,
 
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,10 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: 'Roboto', sans-serif;
+export default createGlobalStyle`
+  body {
+    background: #222;
+    color: #fff;
+    font-family: sans-serif;
   }
 `;
